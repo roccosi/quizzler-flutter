f5284f7a460b5600300874162f90bfe609c62207
roccosi <mibcreative@gmail.com>
2019-06-14 22:57:45 +0200

    Before Sorting scoreKeeper

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -9,7 +12,10 @@ class Quizzler extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey[900],
         body: SafeArea(
-          child: QuizPage(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
         ),
       ),
     );
@@ -22,111 +28,96 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+    setState(() {
+      if (userPickedAnswer == correctAnswer && quizBrain.) {
+        scoreKeeper.add(
+          Icon(
+            Icons.check_circle,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      quizBrain.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          child: Container(),
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
         ),
-        FlatButton(
-          onPressed: null,
-          child: Text('True'),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
         ),
-        FlatButton(onPressed: null, child: Text('False')),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(false);
+                }),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
-
-//class Quizzler extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      home: Scaffold(
-//        backgroundColor: Colors.grey.shade900,
-//        body: SafeArea(
-//          child: Padding(
-//            padding: EdgeInsets.symmetric(horizontal: 10.0),
-//            child: QuizPage(),
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
-//
-//class QuizPage extends StatefulWidget {
-//  @override
-//  _QuizPageState createState() => _QuizPageState();
-//}
-//
-//class _QuizPageState extends State<QuizPage> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Column(
-//      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//      crossAxisAlignment: CrossAxisAlignment.stretch,
-//      children: <Widget>[
-//        Expanded(
-//          flex: 5,
-//          child: Padding(
-//            padding: EdgeInsets.all(10.0),
-//            child: Center(
-//              child: Text(
-//                'This is where the question text will go.',
-//                textAlign: TextAlign.center,
-//                style: TextStyle(
-//                  fontSize: 25.0,
-//                  color: Colors.white,
-//                ),
-//              ),
-//            ),
-//          ),
-//        ),
-//        Expanded(
-//          child: Padding(
-//            padding: EdgeInsets.all(15.0),
-//            child: FlatButton(
-//              textColor: Colors.white,
-//              color: Colors.green,
-//              child: Text(
-//                'True',
-//                style: TextStyle(
-//                  color: Colors.white,
-//                  fontSize: 20.0,
-//                ),
-//              ),
-//              onPressed: () {
-//                //The user picked true.
-//              },
-//            ),
-//          ),
-//        ),
-//        Expanded(
-//          child: Padding(
-//            padding: EdgeInsets.all(15.0),
-//            child: FlatButton(
-//              color: Colors.red,
-//              child: Text(
-//                'False',
-//                style: TextStyle(
-//                  fontSize: 20.0,
-//                  color: Colors.white,
-//                ),
-//              ),
-//              onPressed: () {
-//                //The user picked false.
-//              },
-//            ),
-//          ),
-//        ),
-//        //TODO: Add a Row here as your score keeper
-//      ],
-//    );
-//  }
-//}
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
